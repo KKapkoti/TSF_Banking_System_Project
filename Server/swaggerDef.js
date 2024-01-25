4caec208cb3d9e0d3fc70dd4d7acb1e5b9892235
Kavita kapkoti <115528329+KKapkoti@users.noreply.github.com>
2024-01-25 20:06:43 +0530

    add new file

---
+++ Server/swaggerDef.js
@@ -0,0 +1,44 @@
+// swaggerDef.js
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: number
+ *         name:
+ *           type: string
+ *         balance:
+ *           type: number
+ *     Transaction:
+ *       type: object
+ *       properties:
+ *         sender:
+ *           type: string
+ *         receiver:
+ *           type: string
+ *         amount:
+ *           type: number
+ *         timestamp:
+ *           type: string
+ *
+ * info:
+ *   title: Node.js API with Swagger
+ *   version: 1.0.0
+ *   description: Documentation for Node.js API with Swagger
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   - name: Users
+ *     description: User operations
+ *   - name: Transactions
+ *     description: Transaction operations
+ */
+
+/**
+ * ... (rest of the Swagger definitions)
+ */
